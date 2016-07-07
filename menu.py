3b21cf2c20bdd743805724d4c6d9c5eb66174ed5
marshki <marshall@morethantomorrow.org>
2016-07-07 17:45:45 -0400

    Added menu function

---
+++ menu.py
@@ -0,0 +1,19 @@
+#!/bin/py 	
+# Python 3.4.3 
+# Made with <3 for RNK 
+
+def menu():
+	''' Print menu options and prompt user for input 
+	'''
+	print()
+	print("What would you like to do?")
+	print()
+	print("1) I want to practice random multiplication facts.")
+	print(" ")
+	print("2) I want to practice a single family of multiplication facts.")
+	print(" ") 
+	print("3) I want to quit...for now!")
+	return input("Please make your choice (1,2,3) then press Enter: ")
+()
+
+'''   
