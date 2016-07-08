b47df488e0a333e6dc94faa0cff00d596de1b5e0
marshki <marshall@morethantomorrow.org>
2016-07-08 14:26:07 -0400

    Updated menu function formatting

--- menu.py
+++ menu.py
@@ -3,20 +3,28 @@
 # Made with <3 for RNK 
 
 def menu():
-	''' Print menu options and prompt user for input 
-	'''
-	print()
-	print("What would you like to do?")
-	print()
-	print("1) I want to practice random multiplication facts.")
-	print()
-	print("2) I want to practice a single family of multiplication facts.")
-	print() 
-	print("3) I want to quit...for now!")
-	print()
-	return input("Please make your choice (1,2,3) then press Enter: ")
+	""" Print menu options and prompt user for input 
+	"""
+
+	print("""
+	******************** Multiplication Snap Facts **********************
+
+	This program allows you to practice multiplication facts from 1 to 12. 
+  
+	What would you like to do?
+	
+	(1) I want to practice random multiplication facts.
+	
+	(2) I want to practice a single family of multiplication facts.
+
+	(3) I want to quit...for now. 
+
+	""")
+	return input("	Please make your choice (1,2,3) then press Enter: ")
 () 
 
+# def random_practice(): 
+	
 # Program 
 loop = 1 
 choice = 0 
@@ -24,14 +32,14 @@ while loop == 1:
 	choice = menu()
 	if choice == '1':
 		print()
-		print("OK! Let\'s practice random multiplication facts.")
+		print("	OK! Let\'s practice random multiplication facts.")
 	elif choice == '2': 
 		print()
-		print("Cool! Let\'s practice a single family of multiplication facts.")
+		print("	Cool! Let\'s practice a single family of multiplication facts.")
 	elif choice == '3':
 		print()
-		print("Y\'all come back now, ya hear?") 
+		print("	Y\'all come back now, ya hear?") 
 		loop = 0 
 	else: 
 		print()
-		print("*** Sorry, you must select a valid option (1,2,3), then press Enter. Try again. ***")
+		print("	*** Sorry, you must select a valid option (1,2,3), then press Enter. Try again. ***")
