e179cdab2cc7041c89f5becd3a12882c3b7f7262
marshki <marshall@morethantomorrow.org>
2016-07-08 13:34:24 -0400

    Updated menu function, will eventually merge

--- menu.py
+++ menu.py
@@ -9,11 +9,29 @@ def menu():
 	print("What would you like to do?")
 	print()
 	print("1) I want to practice random multiplication facts.")
-	print(" ")
+	print()
 	print("2) I want to practice a single family of multiplication facts.")
-	print(" ") 
+	print() 
 	print("3) I want to quit...for now!")
+	print()
 	return input("Please make your choice (1,2,3) then press Enter: ")
-()
+() 
 
-'''   
+# Program 
+loop = 1 
+choice = 0 
+while loop == 1: 
+	choice = menu()
+	if choice == '1':
+		print()
+		print("OK! Let\'s practice random multiplication facts.")
+	elif choice == '2': 
+		print()
+		print("Cool! Let\'s practice a single family of multiplication facts.")
+	elif choice == '3':
+		print()
+		print("Y\'all come back now, ya hear?") 
+		loop = 0 
+	else: 
+		print()
+		print("*** Sorry, you must select a valid option (1,2,3), then press Enter. Try again. ***")
