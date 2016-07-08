2c1f4529d51245d79aa5aaee84d72248727a149b
marshki <marshall@morethantomorrow.org>
2016-07-08 14:56:39 -0400

    Made placeholder for function to be included in re-write

---
+++ random_multiply.py
@@ -0,0 +1,8 @@
+#!/bin/py
+# Re-writing original program to include functions
+# This function will draw upon original logic and be incorporated in to re-write 
+
+def random_facts():
+	""" Generate random numbers, multiply those numbers, etc. 
+	"""
+()
