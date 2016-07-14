550d597b01ebd897fdd96e32971782900f29d667
marshki <marshall@morethantomorrow.org>
2016-07-14 13:30:43 -0400

    Updated test_tesy.py

--- test_test.py
+++ test_test.py
@@ -1,14 +1,15 @@
 #!/bin/py
+number = 0 
 while True: 
 	try:
-		number = int(input('Pick a number in range 1-12 >>> '))
+		number = int(input("Enter a whole number between 1 and 12 >>> "))
 	except ValueError:
-		print("That\'s not an int!")
+		print("Invlaid input, please try again >>> ")
 		continue 
 	else: 
-		if not (1<= number <=12): 
-			print('Not in range!')
+		if not (1<= number <=12):
+			print("Need a whole number in range 1-12 >>> ") 
 			continue 
 		else: 
-			print('You selected:',number)
+			print("You selected:",number)
 			break 
